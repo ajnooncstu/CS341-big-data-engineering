5701af15a5f1df07477dd4eb6e03f35fe22b2e5e
ajnooncstu <141294711+ajnooncstu@users.noreply.github.com>
2026-09-16 20:49:22 +0700

    Add analysis questions to SQL preview query
    Added questions for data analysis in SQL preview.

--- session01-trusted-data-product/sql/01_preview_raw.sql
+++ session01-trusted-data-product/sql/01_preview_raw.sql
@@ -6,3 +6,10 @@
 SELECT *
 FROM urban_raw.traffic
 LIMIT 20;
+
+-- Questions:
+--
+-- 1. What columns do you see?
+-- 2. Which columns look like dimensions?
+-- 3. Which columns look like measures?
+-- 4. Do the inferred data types look reasonable?
